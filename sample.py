633f4c72d1c5f19af77f7e3c26e6b883bd644653
sunzhuoshi <sunzhuoshi@gmail.com>
2017-05-22 15:29:13 +0800

    Fix syntax

--- sample.py
+++ sample.py
@@ -10,22 +10,22 @@ KERNEL_BANDWIDTH = 1
 original_points = np.genfromtxt('data.csv', delimiter=',')
 
 #mean shift
-mean_shifer = ms.MeanShift();
-result = mean_shifer.cluster(original_points, KERNEL_BANDWIDTH);
+mean_shifer = ms.MeanShift()
+result = mean_shifer.cluster(original_points, KERNEL_BANDWIDTH)
 shifted_data = result.shifted_points
 
 #init figure
-fig = plt.figure();
+fig = plt.figure()
 
 #original and shifted sub plot
-original_sub_plot = fig.add_subplot(2, 1, 1);
-original_sub_plot.set_title("original + shifted points ");
-original_sub_plot.scatter(original_points[:,0], original_points[:,1], color='r');
-original_sub_plot.scatter(shifted_data[:,0], shifted_data[:,1], color='g');
+original_sub_plot = fig.add_subplot(2, 1, 1)
+original_sub_plot.set_title("original + shifted points ")
+original_sub_plot.scatter(original_points[:,0], original_points[:,1], color='r')
+original_sub_plot.scatter(shifted_data[:,0], shifted_data[:,1], color='g')
 
 #shifted sub plot
-shifted_sub_plot = fig.add_subplot(2, 1, 2);
-shifted_sub_plot.set_title("shifted points");
-shifted_sub_plot.scatter(shifted_data[:,0], shifted_data[:,1], color='g');
+shifted_sub_plot = fig.add_subplot(2, 1, 2)
+shifted_sub_plot.set_title("shifted points")
+shifted_sub_plot.scatter(shifted_data[:,0], shifted_data[:,1], color='g')
 
 plt.show()
